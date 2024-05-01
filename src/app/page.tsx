96c68d51123c81f49ed41a45f888c67471e8ded9
burkoelaborata <kduburko@gmail.com>
2024-04-30 21:56:49 -0300

    Controle de Estado do formulário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { ShieldAlert } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -10,8 +13,32 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Image from "next/image";
+import { useState } from "react";
+
+// interface IError {
+//   hasError: boolean;
+//   message: string;
+// }
 
 export default function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [hasEmailError, setHasEmailError] = useState<string>("");
+  const [hasPasswordError, setHasPasswordError] = useState<string>("");
+
+  function validateForm() {
+    if (email.length === 0) {
+      setHasEmailError("E-mail inválido ou não preenchido!");
+    }
+    if (password.length === 0) {
+      setHasPasswordError("Senha inválida ou não preenchida!");
+    }
+  }
+
+  function handleSubmit() {
+    validateForm();
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8 gap-10">
       <section className="relative w-[15rem] h-[5rem]">
@@ -25,21 +52,55 @@ export default function Login() {
         <CardContent>
           <section className="grid w-full items-center gap-4">
             <section>
-              <Label htmlFor="picture">Email</Label>
-              <Input id="picture" type="text" placeholder="Digite seu e-mail" />
+              <Label
+                htmlFor="picture"
+                className={
+                  hasEmailError !== "" ? "text-red-500" : "text-zinc-500"
+                }>
+                Email
+              </Label>
+              <Input
+                id="picture"
+                type="text"
+                placeholder="Digite seu e-mail"
+                className={hasEmailError !== "" ? "border-red-500" : ""}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              {hasEmailError !== "" && (
+                <section className="text-xs text-red-500 mt-2">
+                  {hasEmailError}
+                </section>
+              )}
             </section>
             <section>
-              <Label htmlFor="picture">Password</Label>
+              <Label
+                htmlFor="picture"
+                className={
+                  hasPasswordError !== "" ? "text-red-500" : "text-zinc-500"
+                }>
+                Password
+              </Label>
               <Input
                 id="picture"
                 type="password"
                 placeholder="Digite sua senha"
+                className={hasPasswordError !== "" ? "border-red-500" : ""}
+                onChange={(event) => setPassword(event.target.value)}
               />
+              {hasPasswordError !== "" && (
+                <section className="text-xs text-red-500 mt-2">
+                  {hasPasswordError}
+                </section>
+              )}
             </section>
           </section>
         </CardContent>
         <CardFooter>
-          <Button className="w-full">Login</Button>
+          <Button
+            className={hasEmailError !== "" ? "!bg-red-500 w-full" : "w-full"}
+            onClick={handleSubmit}>
+            {hasEmailError === "" ? "Login" : <ShieldAlert />}
+          </Button>
         </CardFooter>
       </Card>
     </main>
